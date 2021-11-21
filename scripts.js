8162c16f53b8b573a17a80b86d54a20accd79800
AGarga <agarga@slb.com>
2021-11-21 18:23:36 +0100

    Live price updating

--- scripts.js
+++ scripts.js
@@ -5,15 +5,49 @@ container.setAttribute('class', 'container')
 
 app.appendChild(container)
 
-var request = new XMLHttpRequest()
-request.open('GET', 'https://api.kraken.com/0/public/Ticker?pair=XBTEUR', true)
-request.onload = function() {
-    var data = JSON.parse(this.response)
-    const p = document.createElement('p')
+// var request = new XMLHttpRequest()
+// request.open('GET', 'https://api.kraken.com/0/public/Ticker?pair=XBTEUR', true)
 
+const p = document.createElement('p')
+
+get_da_data = async function() {
+    let result = await makeRequest("GET", 'https://api.kraken.com/0/public/Ticker?pair=XBTEUR');
+    console.log(result)
+
+    // var request = new XMLHttpRequest()
+    // request.open('GET', 'https://api.kraken.com/0/public/Ticker?pair=XBTEUR', true)
+    // console.log(request.status)
+    var data = JSON.parse(result)
+    
     p.textContent = `${data.result.XXBTZEUR.a[0]}`
     console.log(data)
     container.appendChild(p)
+}
+
+var interval_call = setInterval(get_da_data, 500)
+
+function makeRequest(method, url) {
+    return new Promise(function (resolve, reject) {
+        let xhr = new XMLHttpRequest();
+        xhr.open(method, url);
+        xhr.onload = function () {
+            if (this.status >= 200 && this.status < 300) {
+                resolve(xhr.response);
+            } else {
+                reject({
+                    status: this.status,
+                    statusText: xhr.statusText
+                });
+            }
+        };
+        xhr.onerror = function () {
+            reject({
+                status: this.status,
+                statusText: xhr.statusText
+            });
+        };
+        xhr.send();
+    });
 }
 
 // request.onload = function () {
@@ -42,4 +76,4 @@ request.onload = function() {
 //   }
 // }
 
-request.send()
+// request.send()
